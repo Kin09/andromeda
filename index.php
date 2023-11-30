be8b3c59b0ab15a31750090f2f861779df993949
kin09 <kirkin2000@gmail.com>
2023-11-30 07:35:48 -0800

    Your commit message

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Main templete file
+ *
+ * Description.
+ *
+ * @package andromeda
+ */
+
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial ">
+        <meta http-equiv="X-UA-Compatible"content="ie=edge">
+        <title>WordPress Theme</title>
+    </head>
+    <body>
+        Hello WordPress!!
+    </body>
+</html>
